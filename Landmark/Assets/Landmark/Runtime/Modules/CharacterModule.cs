6020e67810c2398289b5b7c844cb0fb1995783c6
JasonChen <138670424@qq.com>
2023-01-03 21:50:33 +0800

    wip:  DebugManager & DebugUI

--- Landmark/Assets/Landmark/Runtime/Modules/CharacterModule.cs
+++ Landmark/Assets/Landmark/Runtime/Modules/CharacterModule.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterModule:MonoBehaviour
+namespace Landmark
 {
-    public List<AnimationClip> AnimationClips = new List<AnimationClip>();
-    public List<GameObject> Landmarks = new List<GameObject>();
-    public SerializableDictionary<int, int[]> Visibility = new SerializableDictionary<int, int[]>();
+    public class CharacterModule : MonoBehaviour
+    {
+        public List<AnimationClip> AnimationClips = new List<AnimationClip>();
+        public List<GameObject> Landmarks = new List<GameObject>();
+        public SerializableDictionary<int, int[]> Visibility = new SerializableDictionary<int, int[]>();
+    }
 }
+
