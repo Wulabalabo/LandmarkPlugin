8674b7caa5b748108170709eab16f27bfcdea788
JasonChen <138670424@qq.com>
2023-01-06 20:30:22 +0800

    feat:  1.Debug UI for scene switching and spawn point switching, animation, Facings  2. Character Inspector can display landmark status by input

--- Landmark/Assets/Landmark/Runtime/DebugManager.cs
+++ Landmark/Assets/Landmark/Runtime/DebugManager.cs
@@ -7,12 +7,13 @@ using UnityEngine;
 namespace Landmark {
     public class DebugManager : MonoBehaviour
     {
+        public string DefaultCharacterName;
         public GameObject CurrentCharacter;
         public LogicScriptable CurrentLogic;
         public List<GameObject> Characters = new List<GameObject>();
         public string CurrentScene;
         public List<Transform> CurrentSpawnPoints = new List<Transform>();
-
+        private List<SkinnedCollisionHelper> _collisionHelpers = new List<SkinnedCollisionHelper>();
 
         internal void Init(List<GameObject> characters)
         {
@@ -20,14 +21,43 @@ namespace Landmark {
             CurrentLogic = GameManager.instance.logicScriptable;
             CurrentScene = CurrentLogic.ScenesLogic.ElementAt(0).Key;
             GenerateSpawnPoints(CurrentScene);
-            GenerateCharacter("Apple_Bianca");
-            
+            GenerateCharacter(DefaultCharacterName);            
+        }
+
+        public void PlayAnimationClip(AnimationClip clip)
+        {
+            clip.SampleAnimation(CurrentCharacter, 0);
+            foreach (var collisionHelper in _collisionHelpers)
+            {
+                collisionHelper.UpdateCollisionMesh();
+            }
+        }
+
+        public void PlayRandomPos()
+        {
+            PoseRandomization.PoseReset();
+            PoseRandomization.ChangePose();
+            foreach (var collisionHelper in _collisionHelpers)
+            {
+                collisionHelper.UpdateCollisionMesh();
+            }
+        }
+
+        public void FacingChange()
+        {
+            CurrentCharacter.transform.Rotate(0, 90, 0);
         }
 
         public void GenerateCharacter(string name)
         {
             DestroyImmediate(CurrentCharacter);
+            
+            _collisionHelpers.Clear();
+
             CurrentCharacter = Instantiate(Characters.Where((item) => item.name == name).First(), parent: GameManager.instance.DebugModePos);
+
+            PoseRandomization.Init(CurrentCharacter);
+
             foreach (Transform transform in CurrentCharacter.GetComponentsInChildren<Transform>())
             {
                 if (transform.CompareTag("CollisionMesh"))
@@ -36,9 +66,9 @@ namespace Landmark {
                     if (transform.name.Equals("CC_Game_Body"))
                     {
                         GameManager.instance.SkinnedCollisionHelper = helper;
-                        Debug.Log("Set");
                     }
                     helper.Init(transform.gameObject);
+                    _collisionHelpers.Add(helper);
                 }
             }
         }
@@ -55,10 +85,10 @@ namespace Landmark {
             CurrentScene = SceneName;
         }
 
-        public IEnumerator QuitDebugMode()
+        public void QuitDebugMode()
         {
+            _collisionHelpers.Clear();
             DestroyImmediate(CurrentCharacter);
-            yield return null;
         }
     }
 }
